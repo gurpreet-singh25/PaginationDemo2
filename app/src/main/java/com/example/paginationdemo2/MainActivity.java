140d27a126673d54ae4c02c1b53d81a5631bc3ab
gurpreet-singh25 <gpsingh5640@gmail.com>
2023-08-09 00:23:40 +0530

    demo

---
+++ app/src/main/java/com/example/paginationdemo2/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.paginationdemo2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
+    ArrayList<Result> arrayList;
+    private static final int pageSize = 50;
+    private int currentPage = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv = findViewById(R.id.rv);
+        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                onResponse();
+            }
+        });
+        arrayList = new ArrayList<>();
+        APIInterface apiInterface = APPClient.getclient().create(APIInterface.class);
+        Call<List<Result>> call = apiInterface.getdata();
+
+        call.enqueue(new Callback<List<Result>>() {
+
+            public boolean isScrollable(){
+                RvAdapter rvAdapter = new RvAdapter(MainActivity.this, arrayList);
+                rv.setAdapter(rvAdapter);
+                return rvAdapter.getItemCount() > currentPage * pageSize;
+            }
+
+
+
+
+             public void onResponse(Call<List<Result>> call, Response<List<Result>> response) {
+                if(isScrollable()){
+                int startPosition = currentPage * pageSize;
+                for (int i = startPosition; i < startPosition + pageSize; i++) {
+                    //response.body().getId();
+                    Result result = new Result();
+                    result.setId(response.body().get(i).getId());
+                    result.setAlbumId(response.body().get(i).getAlbumId());
+                    result.setTitle(response.body().get(i).getTitle());
+                    result.setUrl(response.body().get(i).getUrl());
+                    result.setThumbnailUrl(response.body().get(i).getThumbnailUrl());
+                    arrayList.add(result);
+
+                }
+                currentPage++;
+
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+                rv.setLayoutManager(linearLayoutManager);
+
+            }
+
+
+
+
+
+
+
+        }
+
+            @Override
+            public void onFailure(Call<List<Result>> call, Throwable t) {
+
+            }
+
+        });
+    }
+}
